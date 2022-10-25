bda3dfb2bfff8d51009f6da918e686bea7d0aafb
Jean Damascene CYIFUZO <jeancyifuzodamas@gmail.com>
2022-10-25 23:14:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, decrementBy, increment, incrementByAmount } from "./redux/reducers/counter";
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="justify-center items-center text-center pt-20">
+    <h1 className="mb-10"> The count is: {count}</h1>
+    <button className="p-2 bg-green-500 " onClick={() => dispatch(increment())}>increment</button>
+    <button className="p-2 mx-10 bg-red-500" onClick={() => dispatch(decrement())}>decrement</button>
+    <button className="p-2 bg-green-500" onClick={() => dispatch(incrementByAmount(33))}>
+      Increment by 33
+    </button>
+    <button className="p-2 mx-10 bg-green-500" onClick={() => dispatch(decrementBy(33))}>
+    decrement by 33
+    </button>
+  </div>
   );
 }
 
